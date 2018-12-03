fe8a4c83fa0fa6e2a5f182c3ff32a855e66ebe07
exmczy <2435411376@qq.com>
2018-12-03 15:43:45 +0800

    Update ros_gazebo_install.sh

--- ros_gazebo_install.sh
+++ ros_gazebo_install.sh
@@ -3,17 +3,16 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 sudo apt update
-sudo apt install -y ros-melodic-desktop-full
+sudo apt install ros-lunar-desktop-full
 
 sudo rosdep init
 rosdep update
 
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/lunar/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
 
-. ~/.bashrc
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
 catkin_make
