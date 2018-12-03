156256d40599cce90434777f380fe924b57fe2fd
exmczy <2435411376@qq.com>
2018-12-03 20:48:08 +0800

    Update ros_gazebo_install.sh

--- ros_gazebo_install.sh
+++ ros_gazebo_install.sh
@@ -5,7 +5,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 sudo apt update
-sudo apt install -y ros-melodic-desktop-full
+DEBIAN_FRONTEND=noninteractive sudo apt install -y ros-melodic-desktop-full
 
 sudo rosdep init
 rosdep update
