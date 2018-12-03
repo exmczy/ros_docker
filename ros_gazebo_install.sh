a7abe394691bb09dc4f1df34fd317e5fffd22057
exmczy <2435411376@qq.com>
2018-12-03 15:18:33 +0800

    imodify Dockerfile and install script

--- ros_gazebo_install.sh
+++ ros_gazebo_install.sh
@@ -1,9 +1,3 @@
-apt-get update && \
-  apt-get -y install sudo apt-utils
-
-apt-get install -y vim tmux x11-apps wget
-apt install -y build-essential cmake python3-dev
-
 apt-get install -y lsb-release gnupg
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
