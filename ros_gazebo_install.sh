f2cbe99c7a77abea47ca6f7de4aef90fcfb0ade8
exmczy <2435411376@qq.com>
2018-12-03 15:47:09 +0800

    Update ros_gazebo_install.sh

--- ros_gazebo_install.sh
+++ ros_gazebo_install.sh
@@ -3,7 +3,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 sudo apt update
-sudo apt install ros-lunar-desktop-full
+sudo apt install -y ros-lunar-desktop-full
 
 sudo rosdep init
 rosdep update
@@ -11,7 +11,7 @@ rosdep update
 echo "source /opt/ros/lunar/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
