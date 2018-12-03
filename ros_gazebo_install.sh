bd151cf7434576925667ff111e50bb2a8972ae06
exmczy <2435411376@qq.com>
2018-12-03 22:39:47 +0800

    Update ros_gazebo_install.sh

--- ros_gazebo_install.sh
+++ ros_gazebo_install.sh
@@ -1,5 +1,11 @@
 #!bash
 
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get install -y tzdata
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
 apt-get install -y lsb-release gnupg
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
@@ -16,6 +22,4 @@ source ~/.bashrc
 sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
 
 mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/
-catkin_make
 
